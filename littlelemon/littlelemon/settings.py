9520bd0706595e14cd2307ac1f6ef8383fdae586
geswanel <geswanel@gmail.com>
2024-04-19 19:54:17 +0700

    MySQL db connection using options file

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemon',
+        'OPTIONS': {
+            'read_default_file': str(BASE_DIR / 'db.cnf'),
+        }
     }
 }
 
