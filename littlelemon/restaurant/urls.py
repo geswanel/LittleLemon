1dd9101cda4784ae4a5075ab09b8e82d9438eac8
geswanel <geswanel@gmail.com>
2024-04-27 21:07:44 +0700

    Final touches: index template from static files added

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name="home"),
     path("api-token-auth", obtain_auth_token),
     path("items", views.MenuItemView.as_view(), name="menu_items"),
     path("items/<int:pk>", views.SingleMenuItemView.as_view(), name="menu_single_item"),
