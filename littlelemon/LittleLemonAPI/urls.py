4496cd8b598e7af3075f6b87d704d053a4580f34
geswanel <geswanel@gmail.com>
2024-05-12 02:19:30 +0700

    User management system - little lemon manager workflow

--- littlelemon/LittleLemonAPI/urls.py
+++ littlelemon/LittleLemonAPI/urls.py
@@ -5,7 +5,11 @@ from . import views
 
 app_name = "littlelemon"
 urlpatterns = [
-    path('secure', views.secure_view),
-    path('', include('djoser.urls')),
+    path("secure", views.secure_view),
+    path("", include("djoser.urls")),
+    path("groups/manager/users", views.managers, name="managers"),
+    path("groups/manager/users/<int:pk>", views.delete_manager, name="delete_manager"),
+    path("groups/delivery-crew/users", views.delivery_crew, name="delivery_crew"),
+    path("groups/delivery-crew/users/<int:pk>", views.delete_delivery_crew, name="delete_delivery_crew"),
     # path('', include('djoser.urls.authtoken')),
 ]
